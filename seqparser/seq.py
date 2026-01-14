db263ef162f7369f8124b9ab5568b0e0fae752d9
Keerthi <keerthi.renduchintala@ucsf.edu>
2026-01-13 21:55:14 -0800

    functions for transcribe and reverse_transcribe

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -10,13 +10,24 @@ def transcribe(seq: str, reverse: bool = False) -> str:
     Write a function that will transcribe (replace DNA sequence to RNA
     by replacing all 'T' to 'U') in an input sequence
     """
-    pass
+    transcript = ""
+    # loop through the seq str and based on key in seq add value to transcript string
+    for bp in seq:
+        # if bp is not in ALLOWED_NUC add bp, else add the value matching the key in the dictionary
+        if bp not in ALLOWED_NUC:
+            transcript += bp
+        elif bp in TRANSCRIPTION_MAPPING:
+            transcript += TRANSCRIPTION_MAPPING[bp]
+     # if the read is reversed, reverse
+    if reverse:
+        transcript = transcript[::-1]
+    return transcript
+   
 
 def reverse_transcribe(seq: str) -> str:
     """
     Write a function that will transcribe an input sequence and reverse
     the sequence
     """
-    # Hey this is my comment
-    # Again!
-    pass
+    reverse_transcript = transcribe(seq, True)
+    return reverse_transcript
